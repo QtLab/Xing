fbb33fc7b74d6dae9773824fb706eb3de46f8818
seuki77 <seuki77@gmail.com>
2015-05-30 16:38:01 +0900

    0530-1

--- QtTest/tr/t8430/t8430item.cpp
+++ QtTest/tr/t8430/t8430item.cpp
@@ -1,8 +1,11 @@
 #include "t8430item.h"
+#include "util/fieldutil.h"
 #include <QDebug>
-T8430Item::T8430Item(QObject *parent):TrItem(parent)
+T8430Item::T8430Item(LPt8430OutBlock outblock, QObject *parent):TrItem(parent)
 {
-
+    mHname = GET_STRING_FROM_FIELD(outblock->hname);
+    mShcode = GET_STRING_FROM_FIELD(outblock->shcode);
+    mExpcode = GET_STRING_FROM_FIELD(outblock->expcode);,./
 }
 
 T8430Item::~T8430Item()
