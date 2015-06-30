f20177d92fda91ada3309f881137fd525cf28549
seuki77 <seuki77@gmail.com>
2015-06-30 21:59:25 +0900

    0630-3

--- FundamentalAnalysis/mainwindow.cpp
+++ FundamentalAnalysis/mainwindow.cpp
@@ -1,14 +1,40 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "util/xingutil.h"
+#include <QMessageBox>
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initAction();
+    initMenu();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::updateShcode()
+{
+    QMessageBox::information(this, "debug","updateShcode");
+}
+
+void MainWindow::initAction()
+{
+    mShcodeUpdateAction = new QAction(qkor("종목코드 업데이트"), this);
+    connect(mShcodeUpdateAction, &QAction::triggered, this, &MainWindow::updateShcode);
+
+}
+
+void MainWindow::initMenu()
+{
+    mUpdateMenu = menuBar()->addMenu(tr("Update"));
+    mUpdateMenu->addAction(mShcodeUpdateAction);
+}
+
+void MainWindow::closeEvent(QCloseEvent *event)
+{
+
 }
--- FundamentalAnalysis/mainwindow.h
+++ FundamentalAnalysis/mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QAction>
+#include <QMenu>
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +16,20 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void updateShcode();
+private:
+    void initAction();
+    void initMenu();
+
+protected:
+    virtual void closeEvent(QCloseEvent * event);
+
 private:
     Ui::MainWindow *ui;
+    QMenu* mUpdateMenu;
+
+    QAction* mShcodeUpdateAction;
 };
 
 #endif // MAINWINDOW_H
